b599eb3beb5feac904c0ea4672e135bc5ff912f6
Presley Black <presleyblack24@gmail.com>
2023-06-05 10:40:31 -0500

    created function js exercise

---
+++ variable_exercises.js
@@ -0,0 +1,35 @@
+// exercise 3.1
+let pricePerDayDollar = 3;
+let BrotherBearDays = 5;
+let HerculesDays = 1;
+let littleMermaidDays = 3;
+let movieTotal = (BrotherBearDays + HerculesDays + littleMermaidDays) * pricePerDayDollar;
+
+// exercise 3.2
+let googlePay = 400 * 6;
+let amazonPay = 380 * 4;
+let facebookPay = 350 * 10;
+let totalPayForWeek = googlePay + amazonPay + facebookPay;
+
+// " magical numbers in code", if someone else see code would they know what the numbers are for?
+
+
+
+// exercise 3.3
+let ClassNotFull = true
+let noClassConflict = true
+
+if( ClassNotFull
+      console.log('Good to go!');
+     else {
+      console.log('No go');
+})
+
+// let canenroll = classnotfull === true && noClossConflict === true;
+
+
+// exersice 3.4
+
+
+
+
