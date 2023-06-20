804c59f0a1b393e8c73ecdecba1650f9df0e8d81
Presley Black <presleyblack24@gmail.com>
2023-06-20 09:26:08 -0500

    update html

---
+++ css/flexbox-fundamentals-exercise.css.js
@@ -0,0 +1,210 @@
+* {box-sizing: border-box;}
+
+body
+{
+    margin: 0;
+    padding: 0;
+    background: lightskyblue;
+    font - family
+:
+    Chalkboard, sans - serif;
+}
+
+h1
+{
+    text - align
+:
+    center;
+}
+
+h2, h3
+{
+    padding: 0;
+    margin: 0;
+}
+
+.
+container
+{
+    margin: 0
+    auto;
+    width: 80 %;
+}
+
+.
+green - box
+{
+    background: green;
+    height: 1
+    em;
+    width: 2
+    em;
+}
+
+.
+border
+{
+    border: 2
+    px
+    solid
+    black;
+}
+
+.
+card
+{
+    width: 400
+    px;
+    height: 800
+    px;
+    background: white;
+    padding: 1
+    em;
+    margin: 0
+    auto;
+    /* enter needed declarations here */
+}
+
+section
+{
+    /* enter needed declarations here */
+}
+
+section:nth - of - type(1)
+{
+    height: 3
+    em;
+    background: lightgreen;
+    /* enter needed declarations here */
+}
+section:nth - of - type(1)
+div:last - child
+{
+    /* enter needed declarations here */
+}
+
+.
+purple - box
+{
+    background: fuchsia;
+    width: 3
+    em;
+    height: 1
+    em;
+}
+
+.
+purple - box
+:
+nth - of - type(1)
+{
+    /* enter needed declarations here */
+}
+
+.
+purple - box
+:
+nth - of - type(2)
+{
+    /* enter needed declarations here */
+}
+
+.
+blue - box
+{
+    width: 100 %;
+    background: blue;
+    height: 10
+    em;
+}
+
+section:nth - of - type(3)
+{
+    /* enter needed declarations here */
+}
+
+section:nth - of - type(3)
+h3
+{
+    /* enter needed declarations here */
+
+}
+
+.
+orange - circle
+{
+    background: orange;
+    width: 2
+    em;
+    height: 2
+    em;
+    border - radius
+:
+    1
+    em;
+    margin: .5
+    em;
+}
+
+.
+large - brown - box
+{
+    height: 5
+    em;
+    width: 5
+    em;
+    background: brown;
+    /* enter needed declarations here */
+}
+
+.
+large - brown - box
+:
+nth - of - type(2)
+{
+    margin: 0
+    1
+    em;
+}
+
+section:nth - of - type(5)
+{
+    /* enter needed declarations here */
+}
+
+.
+yellow - box
+{
+    background: yellow;
+    width: 2
+    em;
+    height: 2
+    em;
+}
+
+section:nth - of - type(6)
+{
+    background: coral;
+    height: 14
+    em;
+    /* enter needed declarations here */
+}
+
+.
+grey - box
+{
+    background: grey;
+    width: 3
+    em;
+    height: 3
+    em;
+}
+
+.
+grey - box
+:
+nth - of - type(odd)
+{
+    height: 4
+    em;
+}
